88197184818a7acbce723f3ae5a4f21afb379c01
bath0ry <plylegacy@gmail.com>
2023-02-14 22:10:09 -0300

    add widget header test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:project_api/home/page/view/home_page.dart';
 
 import 'package:project_api/main.dart';
+import 'package:project_api/src/data/ghibli_service.dart';
+import 'package:project_api/src/data/movies_cubit.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home Page has image on header', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(
+      home: MultiBlocProvider(providers: [
+        BlocProvider<MoviesCubit>(
+            create: (BuildContext context) =>
+                MoviesCubit(service: GhibliService(Dio())))
+      ], child: SafeArea(child: HomePage())),
+    ));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
   });
 }
